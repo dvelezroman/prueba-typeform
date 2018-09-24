c32452d3844055941627535baf899003e408da16
Dario <dvelezroman@gmail.com>
2018-09-24 02:22:12 -0300

    boilerplate ready

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import logo from "./logo.svg";
+import { simpleAction } from "./actions/simpleAction";
+import "./styles/App.css";
+
+const mapStateToProps = state => ({
+  ...state
+});
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction())
+});
 
 class App extends Component {
+  simpleAction = event => {
+    this.props.simpleAction();
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,9 +28,14 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <pre>{JSON.stringify(this.props)}</pre>
+        <button onClick={this.simpleAction}>Test redux action</button>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
