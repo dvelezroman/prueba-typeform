8d2f6ca42dfff988a24161d0ec9c791fe74e2a0f
Dario <dvelezroman@gmail.com>
2018-09-28 23:51:50 -0300

    app dockerized

--- server/models/db.js
+++ server/models/db.js
@@ -1,7 +1,9 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("postgres://localhost:5432/typeform", {
+const db = new Sequelize("postgres://postgres@localhost:5432/typeform", {
   logging: false
 });
 
 module.exports = db;
+
+// postgres://todoapp@postgres/todos
