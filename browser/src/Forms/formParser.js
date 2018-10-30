38f5d66a2ac23440952e59f468020df259fc284d
Dario Velez Roman <dvelezroman@gmail.com>
2018-10-30 00:10:22 -0300

    issue fixed

--- browser/src/Forms/formParser.js
+++ browser/src/Forms/formParser.js
@@ -47,7 +47,8 @@ export const createDataForm = (
         type: `${question.type}`,
         properties: {
           description: question.description,
-          steps: question.scale
+          steps: question.scale,
+          start_at_one: true
         }
       };
     } else {
